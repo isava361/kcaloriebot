c4e6e56e94d9c8983853b2122a37191fc65dd81b
Ivan Savelev <iahito361@gmail.com>
2024-03-26 09:25:51 +0000

    Update file handlers.go

--- handlers.go
+++ handlers.go
@@ -225,15 +225,29 @@ func handleMessage(bot *tgbotapi.BotAPI, message *tgbotapi.Message, db *sql.DB) 
             grams := input.Grams
             protein := input.Protein
             fat := input.Fat
-            if protein.Valid && fat.Valid {
-                if (protein.Float64+fat.Float64+carbs) > 100 {
-                    msg := tgbotapi.NewMessage(message.Chat.ID, "Your values for macronutrients add up to more than 100g. Please start again")
-                    msg.ReplyMarkup = defaultkeyboard
-                    bot.Send(msg)
-                    delete(userInputs, userID)
-                    setUserState(userID, stateDefault, db)
-                    return nil
-                }
+            if protein.Valid && fat.Valid && (protein.Float64+fat.Float64+carbs) > 100 {
+                msg := tgbotapi.NewMessage(message.Chat.ID, "Your values for macronutrients add up to more than 100g. Please start again")
+                msg.ReplyMarkup = defaultkeyboard
+                bot.Send(msg)
+                delete(userInputs, userID)
+                setUserState(userID, stateDefault, db)
+                return nil
+            }
+            if !input.Protein.Valid && input.Fat.Valid && input.Fat.Float64+carbs > 100 {
+                msg := tgbotapi.NewMessage(message.Chat.ID, "Fat and carbs values add up to more than 100. Please start again.")
+                msg.ReplyMarkup = defaultkeyboard
+                bot.Send(msg)
+                delete(userInputs, userID)
+                setUserState(userID, stateDefault, db)
+                return nil
+            }
+            if input.Protein.Valid && !input.Fat.Valid && input.Protein.Float64+carbs > 100 {
+                msg := tgbotapi.NewMessage(message.Chat.ID, "Protein and carbs values add up to more than 100. Please start again.")
+                msg.ReplyMarkup = defaultkeyboard
+                bot.Send(msg)
+                delete(userInputs, userID)
+                setUserState(userID, stateDefault, db)
+                return nil
             }
             carbsNull := sql.NullFloat64{Float64: carbs * input.Grams / 100, Valid: true}
             err = addFood(userID, calories*grams/100, grams, protein, fat, carbsNull, db)
